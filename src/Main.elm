71f2d416c3f8c346bc2f7825f8938f0d2e517350
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-08-27 15:17:47 +0100

    Basic 200 mile scale.

--- src/Main.elm
+++ src/Main.elm
@@ -22,7 +22,7 @@ import Svg.Attributes as S
 main : Html msg
 main =
     svg
-        [ viewBox "-5 -10 2010 200"
+        [ viewBox "-100 -10 4100 200"
         , S.width "600px"
         , S.height "100px"
         ]
@@ -34,20 +34,20 @@ rangeTicks =
         tickSize i =
             case ( modBy 10 i, modBy 5 i ) of
                 ( 0, 0 ) ->
-                    40
+                    60
 
                 ( _, 0 ) ->
-                    20
+                    45
 
                 _ ->
-                    10
+                    30
 
         label i =
-            if modBy 10 i == 0 then
+            if i < 200 && modBy 10 i == 0 then
                 [ Svg.text_
-                    [ x (String.fromFloat <| min 1950 <| max 15 (toFloat i * 19.7))
-                    , y "100"
-                    , S.fill "green"
+                    [ x (String.fromFloat   (toFloat i * 20))
+                    , y "-100"
+                    , S.fill "black"
                     , textAnchor "middle"
                     , fontFamily "monospace"
                     , fontSize "60"
@@ -64,12 +64,12 @@ rangeTicks =
                 [ x1 <| String.fromInt (i * 20)
                 , y1 "0"
                 , x2 <| String.fromInt (i * 20)
-                , y2 <| String.fromInt <| tickSize i
-                , stroke "green"
-                , strokeWidth "8"
+                , y2 <| String.fromInt <| 0 - tickSize i
+                , stroke "black"
+                , strokeWidth "6"
                 ]
                 []
             ]
                 ++ label i
     in
-    List.concatMap tick (List.range 0 100)
+    List.concatMap tick (List.range 0 200)
